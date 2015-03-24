080a5ddf6ed9a788c5a32c95582702fd5fb4b7c1
Luke Sheard <me@lukesheard.com>
2015-03-24 01:41:57 -0600

    php testing

--- assets/js/main.js
+++ assets/js/main.js
@@ -79,8 +79,8 @@ $(document).ready(function(){
                   failure: function(){
                     console.log("Failure!");
                   },
-                  error: function(ts) {
-                    console.log(ts.responseText);
+                  error: function(jqXHR, textStatus, errorThrown) {
+                    console.log(textStatus, errorThrown);
                   }
               });
           }
