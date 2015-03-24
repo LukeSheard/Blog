ce96bf8b881f59714ec2832a89fc2d6d3b1fcfa6
Luke Sheard <me@lukesheard.com>
2015-03-24 02:24:35 -0600

    php testing

--- Systems/editLikes.php
+++ Systems/editLikes.php
@@ -1,6 +1,5 @@
 <?php
 header("Access-Control-Allow-Origin: http://www.lukesheard.com");
-header("Access-Control-Allow-Origin: http://127.0.0.1:4000");
 header("Content-Type: application/javascript");
 
 echo $_GET['callback'];
