a17cf206d482e0f224529d071af87028045c8609
Luke Sheard <me@lukesheard.com>
2015-03-24 01:40:35 -0600

    php testing

--- assets/js/main.js
+++ assets/js/main.js
@@ -71,7 +71,7 @@ $(document).ready(function(){
                   type: "POST",
                   // url: "/Systems/editLikes.php",
                   url: "http://systems.lukesheard.com/editLikes.php",
-                  data: phpString,
+                  data: JSON.stringify(phpString),
                   dataType: "jsonp",
                   success: function(data){
                     console.log(data);
