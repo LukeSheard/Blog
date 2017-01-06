54dc5d499b77d004e3e04e7afbab8ba9b395c842
Luke Sheard <me@lukesheard.com>
2017-01-06 07:14:25 +0000

    Initialise basic page

---
+++ gulpfile.js
@@ -0,0 +1,27 @@
+const css  = require('gulp-clean-css')
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const srcm = require('gulp-sourcemaps');
+
+gulp.task('default', [ 'build', 'watch' ]);
+gulp.task('build', [ 'sass', 'assets' ]);
+gulp.task('watch', function() {
+  gulp.watch('./src/**/*.scss', [ 'sass'   ]);
+  gulp.watch('./src/**/*.html', [ 'assets' ]);
+});
+
+gulp.task('assets', function() {
+  return gulp.src('./src/**/*.html')
+    .pipe(gulp.dest('./public'));
+});
+
+gulp.task('sass', function() {
+  return gulp.src('./src/style/index.scss')
+    .pipe(srcm.init())
+    .pipe(sass().on('error', sass.logError))
+    .pipe(css({
+      compatibility: 'ie8'
+    }))
+    .pipe(srcm.write())
+    .pipe(gulp.dest('./public/css'));
+});
