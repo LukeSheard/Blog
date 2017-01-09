52e6d75857e2fb31b5b5273f3b211fe58be12e8d
Luke Sheard <me@lukesheard.com>
2017-01-09 21:12:26 +0000

    Add icons

--- gulpfile.js
+++ gulpfile.js
@@ -7,12 +7,19 @@ gulp.task('default', [ 'build', 'watch' ]);
 gulp.task('build', [ 'sass', 'assets' ]);
 gulp.task('watch', function() {
   gulp.watch('./src/**/*.scss', [ 'sass'   ]);
-  gulp.watch('./src/**/*.html', [ 'assets' ]);
+  gulp.watch([
+    './src/**/*',
+    '!./src/style',
+    '!./src/style/**/*'
+  ], [ 'assets' ]);
 });
 
 gulp.task('assets', function() {
-  return gulp.src('./src/**/*.html')
-    .pipe(gulp.dest('./public'));
+  return gulp.src([
+    './src/**/*',
+    '!./src/style',
+    '!./src/style/**/*'
+  ]).pipe(gulp.dest('./public'));
 });
 
 gulp.task('sass', function() {
